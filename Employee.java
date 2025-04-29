665ae4c1b8c4d5a5cb2d0f15fd6f95871e9a32a1
rohitvk09 <justlogin49@gmail.com>
2025-04-29 08:47:38 +0530

    first commit

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package Devops.Employee;
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+
+    }
+
+    public void displayInfo(){
+        System.out.println("Employee ID:" + id);
+        System.out.println("Employee Name:" + name);
+        System.out.println("Employee Salary:" + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Rohit", 108,20000);
+        emp1.displayInfo();
+    }
+    
+}
